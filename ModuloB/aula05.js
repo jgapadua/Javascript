3c763b19aa8573688dee39080a93dd4af70e81aa
jgapadua <joaogabrielpadua@gmail.com>
2023-04-27 12:36:18 -0300

    finalizei primeira aula

---
+++ ModuloB/aula05.js
@@ -0,0 +1,11 @@
+/*Tipos de variaveis: number -> Infinity, NaN
+ string, boolean, null, undefined, 
+ object -> Array 
+ function
+ */
+//
+var idade = 22;
+var nome = "João"
+var cidade = 'Belo Horizonte'
+var empresa = `Meta3`
+typeof 22;
