1eec9a1169720e8706296913f63f4a873cc67cd9
jgapadua <joaogabrielpadua@gmail.com>
2023-04-28 14:31:16 -0300

    finalizei exercicios

--- ModuloE/aula14ex/ex18/script.js
+++ ModuloE/aula14ex/ex18/script.js
@@ -1,1 +1,18 @@
-alert('Olá')
+function gerar() {
+  var numero = document.getElementById('num')
+  var tabuada = document.getElementById('tabuada')
+  if (numero.value.length == 0) {
+      window.alert( 'digite um número')
+  } else {
+      var n = Number(numero.value)
+      var c = 1
+      tabuada.innerHTML= ''
+      while( c <= 10){
+          var item = document.createElement('option')
+          item.text = `${n} x ${c} = ${n*c}`
+          tabuada.appendChild(item)
+          c++
+      }
+  }
+
+}
