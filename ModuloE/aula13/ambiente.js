7915871c7b2e74a4f3ecc709821e80253dfec23f
jgapadua <joaogabrielpadua@gmail.com>
2023-04-28 11:40:47 -0300

    comecei modulo

---
+++ ModuloE/aula13/ambiente.js
@@ -0,0 +1,29 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+/*var c = 10
+p = 2
+while (c >= 1) {
+    console.log(`Passo ${c}`)  << DECRESCENTE
+    c-=p
+}
+*/
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
+
+var c = 1 
+do {
+    console.log(`teste ${c}`)
+    c++
+}
+while (c <=12)
