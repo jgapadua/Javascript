0819f5f8275d5b8176d139a055d7c644594208dd
jgapadua <joaogabrielpadua@gmail.com>
2023-04-28 09:13:30 -0300

    finalizei aula 11

---
+++ ModuloD/aula11/ex09.js
@@ -0,0 +1,7 @@
+var pais = 'Canada'
+    console.log (`vivendo em ${pais}`)
+    if(pais == 'Brasil') {
+        console.log('Voce é Estrangeiro!')
+    } else {
+        console.log ('Voce é Brasileiro!')
+    }
