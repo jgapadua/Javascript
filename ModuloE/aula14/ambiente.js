8beeb9d55c170aee60a039657f3c29fe365656bb
jgapadua <joaogabrielpadua@gmail.com>
2023-04-28 11:50:50 -0300

    finalizei aulas

---
+++ ModuloE/aula14/ambiente.js
@@ -0,0 +1,28 @@
+/*
+var c = 1
+while (c <= 5) {
+    console.log(c)
+    c++
+}
+*/
+
+//---------------------
+console.log('Vai começar...')
+for (var c = 1 ;c <= 5; c++) {
+    console.log(c)
+
+}
+console.log('FIM!')
+
+
+console.log('Starting')
+var c = 1
+while (c <= 5 ) {
+    console.log(c)
+    c++
+}
+
+for (var c = 1 ; c <= 8; c++) {
+    console.log (c)
+}
+console.log ('End')
